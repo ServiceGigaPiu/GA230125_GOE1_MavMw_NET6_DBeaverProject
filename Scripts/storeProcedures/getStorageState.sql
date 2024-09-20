263430e8e5aa61faffa9d9b65848f7de4a0c6d24
ServiceGigaPiu <service@gigapiu.it>
2024-09-20 17:00:21 +0200

    *getStorageState
    aggiunta condizione per scaffale in errore

--- Scripts/storeProcedures/getStorageState.sql
+++ Scripts/storeProcedures/getStorageState.sql
@@ -1,7 +1,7 @@
 delimiter $$
 
 DROP PROCEDURE IF EXISTS `getStorageState` $$
-CREATE DEFINER=`readonly`@`%` PROCEDURE `devdb`.`getStorageState`(
+CREATE DEFINER=`readonly`@`%` PROCEDURE `getStorageState`(
 
     IN laneIdxString VARCHAR(10), -- 01 / 02 .... 06
     IN laneSideString VARCHAR(10) -- L / R
@@ -23,7 +23,7 @@ BEGIN
             LEFT JOIN activity ON activity.id = activity_uses_repository.activity_id
             LEFT JOIN repositoryCoordinateRaw ON repositoryCoordinateRaw.repo_id = repository.id
             WHERE (repository_hasmany_ingoing.id IS NOT NULL OR repository_hasmany_outgoing.id IS NOT NULL)
-              AND (activity.run_state_enum > 0 AND activity.run_state_enum < 5)
+              AND (activity.run_state_enum >= 0 AND activity.run_state_enum < 5)
               AND repositoryCoordinateRaw.laneIdx = laneIdxString
               AND repositoryCoordinateRaw.sideOfLane = laneSideString
         ) INTO gialla;
@@ -39,21 +39,30 @@ BEGIN
             LEFT JOIN activity ON activity.id = activity_uses_repository.activity_id
             LEFT JOIN repositoryCoordinateRaw ON repositoryCoordinateRaw.repo_id = repository.id
             WHERE (repository_hasmany_ingoing.id IS NOT NULL OR repository_hasmany_outgoing.id IS NOT NULL)
-              AND activity.run_state_enum IS NULL
+              and (activity.run_state_enum IS null or activity.run_state_enum >= 5)
               AND repositoryCoordinateRaw.laneIdx = laneIdxString
               AND repositoryCoordinateRaw.sideOfLane = laneSideString
         ) INTO arancio;
-
+	
     -- Valutazione finale con IF
-    IF gialla = 1 THEN
-        SELECT 2 AS state;  -- Stato Giallo
-    ELSEIF arancio = 1 THEN
-        SELECT 3 AS state;  -- Stato Arancio
+    IF arancio = 1 THEN
+        SELECT 3 AS state;  -- Stato Giallo
+    ELSEIF gialla = 1 THEN
+        SELECT 2 AS state;  -- Stato Arancio
     ELSE
         SELECT 1 AS state;  -- Stato Celeste
     END IF;
-
 END
 
 $$
 delimiter ;
+
+SELECT *
+            FROM repository
+            LEFT JOIN repository_hasmany_ingoing ON repository_hasmany_ingoing.repository_id = repository.id
+            LEFT JOIN repository_hasmany_outgoing ON repository_hasmany_outgoing.repository_id = repository.id
+            LEFT JOIN activity_uses_repository ON activity_uses_repository.repository_id = repository.id
+            LEFT JOIN activity ON activity.id = activity_uses_repository.activity_id
+            LEFT JOIN repositoryCoordinateRaw ON repositoryCoordinateRaw.repo_id = repository.id
+            WHERE (repository_hasmany_ingoing.id IS NOT NULL OR repository_hasmany_outgoing.id IS NOT NULL)
+              AND (activity.run_state_enum IS null or activity.run_state_enum >= 5)
