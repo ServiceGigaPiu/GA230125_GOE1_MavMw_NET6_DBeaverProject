f31d3d8a3609e8baf7702ac4e7e6a1efe7d5e76c
ServiceGigaPiu <service@gigapiu.it>
2024-10-08 16:50:18 +0200

    select di GetAllMissions

--- Scripts/storeProcedures/GetAllMissions.sql
+++ Scripts/storeProcedures/GetAllMissions.sql
@@ -4,15 +4,12 @@ DROP PROCEDURE IF EXISTS `getAllMissions` $$
 CREATE DEFINER=`readonly`@`%` PROCEDURE `getAllMissions`()
 begin
 	SELECT 
-	tag, 
-	run_state_enum, 
-	error_code, 
-	accept_dt, 
-	start_dt, 
-	end_dt, 
-	expire_dt, 
-	scheduled_order
-	FROM devdb.activity;
+	_id, 
+	repo_tag, 
+	udc_tag, 
+	run_state, 
+	ok
+	FROM activitystatus a ;
 END
 
 $$
